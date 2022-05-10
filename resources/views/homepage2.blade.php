2af2e740b93e5dd2a47c03251c3047134f1688aa
Chavalit Koweerawong <chavalit.kow@gmail.com>
2022-05-10 22:35:15 +0700

    Initial commit

---
+++ resources/views/homepage2.blade.php
@@ -0,0 +1,188 @@
+<!-- 
+THEME: Small Apps | Bootstrap App Landing Template
+VERSION: 1.0.0
+AUTHOR: Themefisher
+
+HOMEPAGE: https://themefisher.com/products/small-apps-free-app-landing-page-template/
+DEMO: https://demo.themefisher.com/small-apps/
+GITHUB: https://github.com/themefisher/Small-Apps-Bootstrap-App-Landing-Template
+
+Get HUGO Version : https://themefisher.com/products/small-apps-hugo-app-landing-theme/
+
+WEBSITE: https://themefisher.com
+TWITTER: https://twitter.com/themefisher
+FACEBOOK: https://www.facebook.com/themefisher
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Basic Page Needs
+  ================================================== -->
+  <meta charset="utf-8">
+  <title>Banglen | รู้ทุกเรื่องเกี่ยวกับบางเลน</title>
+
+  <!-- Mobile Specific Metas
+  ================================================== -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Bootstrap App Landing Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+  <meta name="author" content="Themefisher">
+  <meta name="generator" content="Themefisher Small Apps Template v1.0">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="{{ asset('small-apps/images/favicon.png')}}" />
+
+  <!-- PLUGINS CSS STYLE -->
+  <link rel="stylesheet" href="{{ asset('small-apps/plugins/bootstrap/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('small-apps/plugins/themify-icons/themify-icons.css')}}">
+  <link rel="stylesheet" href="{{ asset('small-apps/plugins/slick/slick.css')}}">
+  <link rel="stylesheet" href="{{ asset('small-apps/plugins/slick/slick-theme.css')}}">
+  <link rel="stylesheet" href="{{ asset('small-apps/plugins/fancybox/jquery.fancybox.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('small-apps/plugins/aos/aos.css')}}">
+
+  <!-- CUSTOM CSS -->
+  <link href="{{ asset('small-apps/css/style.css')}}" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
+  <style>
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    nav,
+    .nav,
+    .menu,
+    button,
+    .button,
+    .btn,
+    .price,
+    ._heading,
+    .wp-block-pullquote blockquote,
+    blockquote,
+    label,
+    legend,
+    a,
+    .card-header,
+    th,
+    p,
+    li {
+      font-family: "Prompt", "Open Sans script=all rev=1" !important;
+      font-weight: 500 !important;
+
+    }
+  </style>
+
+</head>
+
+<body class="body-wrapper" data-spy="scroll" data-target=".privacy-nav">
+
+
+  <!--=============================            Banner            ==============================-->
+  <div class="section banner-full">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 justify-content-xs-center order-lg-first order-md-last order-last">
+          <div class="image" data-aos="flip-right">
+            <!-- <img class="img-fluid" src="{{asset('small-apps/images/feature/iphone-black.jpg')}}" alt="Iphone-Black"> -->
+            <!-- <img class="img-fluid" src="{{asset('small-apps/images/feature/feature-new-01.jpg')}}" alt="Iphone-Black"> -->
+            <img class="" src="{{asset('images/feature-new-01-custom.png')}}"  style="height: auto;" alt="Iphone-Black">
+          </div>
+        </div>
+        <div class="col-lg-8 align-self-center">
+          <div class="block px-4 py-4">
+            <div class="logo">
+              <a href="index.html"><img src="{{asset('images/logo-green.png')}}" style="border-radius: 10%;" height="100" alt="logo"></a>
+            </div>
+            <!-- <h1>Introducing Game Changing
+              <br>Lifestyle Apps
+            </h1> -->
+            <h1>Banglen App <br> รู้ทุกเรื่องเกี่ยวกับบางเลน </h1>
+            <p>
+              แอพพลิเคชั่น รวบรวมข้อมูลปัจจัยการเพาะปลูกในอำเภอบางเลน และข้อมูลราคา และตลาด เพื่อให้ผู้ประกอบการนำไปประกอบการตัดสินใจในการดำเนินธุรกิจด้านเกษตรกรรม
+            </p>
+            <ul class="list-inline app-badge">
+              <li class="list-inline-item">
+                <a href="https://apps.apple.com/th/app/%E0%B8%9A%E0%B8%B2%E0%B8%87%E0%B9%80%E0%B8%A5%E0%B8%99/id1620104144">
+                  <img src="{{asset('images/appstore_en.png')}}" alt="Apple Store">
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://play.google.com/store/apps/details?id=com.rtfloodbma.banglen">
+                  <img src="{{asset('images/googleplaystore_en.png')}}" alt="Google Play">
+                </a>
+              </li>
+            </ul>
+            <div class="support">
+              <!-- <img class="img-fluid" src="{{asset('small-apps/images/icons/supported-services.png')}}" alt="supported-services"> -->
+              <img class="img-fluid" src="{{asset('images/banglen-website.png')}}" style="width: 150px;" alt="บางเลนเว็บไซต์">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--====  End of Banner  ====-->
+
+  <!--=============================            Footer            ==============================-->
+
+  <footer class="footer-classic">
+    <ul class="social-icons list-inline">
+      <li class="list-inline-item">
+        <a href="https://www.facebook.com/themefisher"><i class="ti-apple"></i></a>
+      </li>
+      <li class="list-inline-item">
+        <a href="https://twitter.com/themefisher"><i class="ti-android"></i></a>
+      </li>
+      <li class="list-inline-item">
+        <a href="https://medium.com/banglen-th"><img src="{{ asset('images/medium.png') }}" style="vertical-align: initial;"  height="25" alt="บล็อคบทความ"></a>
+      </li>
+    </ul>
+    <ul class="footer-links list-inline">
+      <li class="list-inline-item">
+        <a href="https://apps.apple.com/th/app/%E0%B8%9A%E0%B8%B2%E0%B8%87%E0%B9%80%E0%B8%A5%E0%B8%99/id1620104144">App Store</a>
+      </li>      
+      <li class="list-inline-item">
+        <a href="https://play.google.com/store/apps/details?id=com.rtfloodbma.banglen">Google Play Store</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="https://medium.com/banglen-th">Blog</a>
+      </li>
+      <!-- <li class="list-inline-item">
+        <a href="privacy-policy.html">Privacy</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="team.html">Developer</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="contact.html">Support</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="career.html">Career</a>
+      </li> -->
+    </ul>
+  </footer>
+
+
+  <!-- To Top -->
+  <div class="scroll-top-to">
+    <i class="ti-angle-up"></i>
+  </div>
+
+  <!-- JAVASCRIPTS -->
+  <script src="{{asset('small-apps/plugins/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('small-apps/plugins/bootstrap/bootstrap.min.js')}}"></script>
+  <script src="{{asset('small-apps/plugins/slick/slick.min.js')}}"></script>
+  <script src="{{asset('small-apps/plugins/fancybox/jquery.fancybox.min.js')}}"></script>
+  <script src="{{asset('small-apps/plugins/syotimer/jquery.syotimer.min.js')}}"></script>
+  <script src="{{asset('small-apps/plugins/aos/aos.js')}}"></script>
+  <!-- google map -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAgeuuDfRlweIs7D6uo4wdIHVvJ0LonQ6g"></script>
+  <script src="{{asset('small-apps/plugins/google-map/gmap.js')}}"></script>
+
+  <script src="{{asset('small-apps/js/script.js')}}"></script>
+</body>
+
+</html>
